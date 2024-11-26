557d3b3554a79b1ded137bdee625af6a3bdaa6db
Hai Nghiem <hainghiem375@gmail.com>
2024-11-26 03:18:51 -0500

    fix: small tweaks

--- projects/web_search_battle/reference.py
+++ projects/web_search_battle/reference.py
@@ -15,9 +15,18 @@ ground_truths = [
         "input": "Who won the fight between Jake Paul and Mike Tyson?",
         "output": "Jake Paul won the fight",
     },
-    {"input": "Who's the CTO of Vectorize.io?", "output": "Chris Bartholomew"},
-    {"input": "Who's the CEO of Quotient AI?", "output": "Julia Neagu"},
-    {"input": "Who's the CEO of Invest Ottawa now?", "output": "Sonya Shorey"},
+    {
+        "input": "Who's the CTO of Vectorize.io?",
+        "output": "Chris Bartholomew is the CTO and co-founder of Vectorize.io",
+    },
+    {
+        "input": "Who's the CEO of Quotient AI?",
+        "output": "Julia Neagu is the CEO and co-founder of Quotient AI",
+    },
+    {
+        "input": "Who's the CEO of Invest Ottawa now?",
+        "output": "Sonya Shorey is the CEO of Invest Ottawa currently",
+    },
     {
         "input": "When did AI Tinkerers Ottawa have their last event in 2024",
         "output": "November 19th 2024",
